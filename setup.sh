6992b30d1a0ac5eba3539ed4673c3317d9ebe812
shailesh-zx <138345328+zx-hackers@users.noreply.github.com>
2023-10-27 19:49:12 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,11 @@
+apt update && apt upgrade -y
+mv parrot /data/data/com.termux/files/usr/bin
+chmod +x /data/data/com.termux/files/usr/bin/parrot
+cd
+mv parrot-in-termux parrot
+cd parrot
+bash setup.sh
+./startparrot.sh apt update
+apt upgrade -y
+apt install parrot-desktop-mate
+bash desktop.sh
